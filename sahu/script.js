aaeb83c68e5d1ba39d6ad25f184067256897c6ae
caro-kei <caropna@hotmail.com>
2026-04-05 11:44:36 -0300

    Corregido carrusel y añadidas imagenes de revendedor

--- sahu/script.js
+++ sahu/script.js
@@ -1,11 +1,22 @@
 function filtrar(categoria) {
-  const productos = document.querySelectorAll(".producto");
+    const productos = document.querySelectorAll(".producto");
 
-  productos.forEach(p => {
-    if (categoria === "todos") {
-      p.style.display = "block";
-    } else {
-      p.style.display = p.classList.contains(categoria) ? "block" : "none";
-    }
-  });
+    productos.forEach(p => {
+        if (categoria === "todos") {
+            p.style.display = "block";
+        } else {
+            p.style.display = p.classList.contains(categoria) ? "block" : "none";
+        }
+    });
 }
+
+let index = 0;
+const imagenes = document.querySelectorAll(".carrusel img");
+
+function cambiarImagen() {
+    imagenes[index].classList.remove("activo");
+    index = (index + 1) % imagenes.length;
+    imagenes[index].classList.add("activo");
+}
+
+setInterval(cambiarImagen, 3000); // cambia cada 3 segundos
